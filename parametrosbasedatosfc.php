c5db161c1be6e3f8b9a4cbd877dc310fd888df0b
Emmanuel Carballo <zurdoemma@hotmail.com>
2019-04-28 22:59:17 -0300

    Agregado de tiempo de sesion

--- parametrosbasedatosfc.php
+++ parametrosbasedatosfc.php
@@ -4,7 +4,4 @@
    $db_name = "finan_cli";
    $db_password = "21072112";
    $db_port = "3306";
-   $usuario_correo="opecen@libertadsa.com.ar";
-   $clave_correo="Nada";
-   $usuario_send="Informes Libertad";
 ?>
