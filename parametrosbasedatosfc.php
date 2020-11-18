6f14bc12dab42af670bf7703f23719ba9a970228
Emmanuel CARBALLO <zurdoemma@hotmail.com>
2020-11-18 19:35:19 -0300

    Update

--- parametrosbasedatosfc.php
+++
@@ -1,7 +0,0 @@
-<?php
-   $serverName = "localhost";
-   $db_user = "admin_fc";
-   $db_name = "finan_cli";
-   $db_password = "21072112";
-   $db_port = "3306";
-?>
--- parametrosbasedatosta.php
+++
@@ -1,7 +0,0 @@
-<?php
-   $serverName = "10.147.150.19";
-   $db_user = "tefcon";
-   $db_name = "tef";
-   $db_password = "21072112";
-   $db_port = "3306";
-?>
